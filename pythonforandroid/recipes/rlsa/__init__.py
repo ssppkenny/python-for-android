139fcc121bbfc593b7b9b091c6330f60a24058ba
Sergey Mikhno <sergey.mikhno@galenica.com>
2024-12-01 13:51:11 +0100

    rlsa

---
+++ pythonforandroid/recipes/rlsa/__init__.py
@@ -0,0 +1,16 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+
+class RlsaRecipe(CompiledComponentsPythonRecipe):
+    """This is a two-in-one recipe:
+      - build labraru `libprotobuf.so`
+      - build and install python binding for protobuf_cpp
+    """
+    name = 'rlsa'
+    version = '0.0.1'
+    url = 'https://github.com/ssppkenny/rlsa/archive/refs/tags/{version}.tar.gz'
+    call_hostpython_via_targetpython = False
+    depends = ['numpy', 'setuptools']
+    site_packages_name = 'rlsa'
+
+recipe = RlsaRecipe()
